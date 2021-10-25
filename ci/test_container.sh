c37145fa586dbdfaf97df8866f0a3a517d653be5
Alberto Alvarez <albertoa@nvidia.com>
2021-10-25 11:16:12 -0700

    21.09 dlfw and cudf 20.10 (#54)
    * Changes to nightly
    
    * Changes HugeCTR install
    
    * Adds package and removes npm upgrades
    
    * S3 Support
    
    * Disables cudf S3
    
    * Fixes typo
    
    * Fixes ibverbs and splog
    
    * Fixes spdlog
    
    * Fixes tf4rec install
    
    * Fixes vuls
    
    * Updates HugeCTR and scan
    
    * Updates HugeCTR
    
    * Updates Arrow
    
    * Updates Paths
    
    * Temporary changes NVTabular repo
    
    * Removes path
    
    * Updates training
    
    * Adds testing script
    
    * Permission
    
    * Updates testing script
    
    * cudf 21.10
    
    * Updates to latest
    
    * fix tf image building; use HUGECTR_VER as branch name
    
    * change hugectr version
    
    * Updates cudf branch
    
    * Updates to main NVTabular
    
    * Update NVTabular docker/dockerfile.torch
    
    Co-authored-by: Ben Frederickson <github@benfrederickson.com>
    
    * Updates distributed
    
    * Update dockerfile.tf
    
    * adds daemon file
    
    Co-authored-by: aleliu <aleliu@nvidia.com>
    Co-authored-by: Ben Frederickson <github@benfrederickson.com>
    Co-authored-by: XiaoleiShi-NV <70248893+XiaoleiShi-NV@users.noreply.github.com>

---
+++ ci/test_container.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+container=$1
+
+# Test NVTabular
+pytest /nvtabular/tests/unit
+
+# Test HugeCTR
+if [ "$container" == "merlin-training" ]; then
+    layers_test && \
+    checker_test && \
+    data_reader_test && \
+    device_map_test && \
+    loss_test && \
+    optimizer_test && \
+    regularizers_test && \
+    model_oversubscriber_test && \
+    parser_test && \
+    auc_test
+elif [ "$container" == "merlin-tensorflow-training" ]; then
+    embedding_test
+elif [ "$container" == "merlin-inference" ]; then
+    inference_test
+fi
+
+# Test Transformers4Rec
+if [ "$container" != "merlin-training" ]; then
+    pytest /transformers4rec/tests/unit
+fi
+
--- docker/dockerfile.tf
+++ docker/dockerfile.tf
@@ -1,5 +1,5 @@
 # syntax=docker/dockerfile:1
-ARG IMAGE=nvcr.io/nvidia/tensorflow:21.07-tf2-py3
+ARG IMAGE=nvcr.io/nvidia/tensorflow:21.09-tf2-py3
 FROM ${IMAGE} AS phase1
 ENV CUDA_SHORT_VERSION=11.4
 
@@ -9,10 +9,11 @@ ENV LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extra
 ENV DEBIAN_FRONTEND=noninteractive
 
 ARG RELEASE=false
-ARG RMM_VER=v21.08.00
-ARG CUDF_VER=v21.08.01
+ARG RMM_VER=vnightly
+ARG CUDF_VER=vnightly
 ARG NVTAB_VER=vnightly
-ARG HUGECTR_VER=v21.9
+ARG TF4REC_VER=vnightly
+ARG HUGECTR_VER=vnightly
 ARG SM="60;61;70;75;80"
 
 ENV CUDA_HOME=/usr/local/cuda
@@ -27,15 +28,11 @@ RUN apt update -y --fix-missing && \
     apt upgrade -y && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
         vim gdb git wget unzip tar \ 
-        #python3.8-dev \
-        zlib1g-dev lsb-release clang-format libboost-all-dev \
+        zlib1g-dev lsb-release clang-format libboost-serialization-dev \
         openssl curl zip\
         libssl-dev \
         protobuf-compiler \
-        numactl \
-        libnuma-dev \
         libaio-dev \
-        libibverbs-dev \
         slapd && \
       apt install -y --no-install-recommends software-properties-common && \
       add-apt-repository -y ppa:deadsnakes/ppa && \
@@ -43,18 +40,20 @@ RUN apt update -y --fix-missing && \
 
 RUN pip install git+git://github.com/gevent/gevent.git@21.8.0#egg=gevent
 
-RUN pip install 'notebook>=6.4.1' 'sqlparse>=0.4.2' 'websockets>=10.0'
-RUN ln -s /usr/local/nvm/versions/node/v15.12.0/bin/node /usr/bin/node
-RUN /usr/local/nvm/versions/node/v15.12.0/bin/npm -g upgrade
-RUN rm -rf /usr/local/share/jupyter/lab/staging/node_modules/fast-json-patch
-
 # Install cmake
 RUN apt remove --purge cmake -y && wget http://www.cmake.org/files/v3.21/cmake-3.21.1.tar.gz && \
     tar xf cmake-3.21.1.tar.gz && cd cmake-3.21.1 && ./configure && make && make install
 
+# Install spdlog from source
+RUN git clone --branch v1.9.2 https://github.com/gabime/spdlog.git build-env && \
+    pushd build-env && \
+      mkdir build && cd build && cmake .. && make -j && make install && \
+    popd && \
+    rm -rf build-env
+
 # Install arrow from source
 ENV ARROW_HOME=/usr/local
-RUN git clone --branch apache-arrow-4.0.1 --recurse-submodules https://github.com/apache/arrow.git build-env && \
+RUN git clone --branch apache-arrow-5.0.0 --recurse-submodules https://github.com/apache/arrow.git build-env && \
     pushd build-env && \
       export PARQUET_TEST_DATA="${PWD}/cpp/submodules/parquet-testing/data" && \
       export ARROW_TEST_DATA="${PWD}/testing/data" && \
@@ -98,8 +97,8 @@ RUN git clone --branch apache-arrow-4.0.1 --recurse-submodules https://github.co
 FROM phase1 as phase2
 
 ARG RELEASE=false
-ARG RMM_VER=v21.08.00
-ARG CUDF_VER=v21.08.01
+ARG RMM_VER=vnightly
+ARG CUDF_VER=vnightly
 
 # Install rmm from source
 RUN git clone https://github.com/rapidsai/rmm.git build-env && cd build-env/ && \
@@ -115,14 +114,14 @@ RUN git clone https://github.com/rapidsai/rmm.git build-env && cd build-env/ && 
 
 # Build env for CUDF build
 RUN git clone https://github.com/rapidsai/cudf.git build-env && cd build-env/ && \
-    if [ "$RELEASE" == "true" ] && [ ${CUDF_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${CUDF_VER}; else git checkout main; fi; \
+    if [ "$RELEASE" == "true" ] && [ ${CUDF_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${CUDF_VER}; else git checkout branch-21.10; fi; \
     git submodule update --init --recursive && \
     cd .. && \
     pushd build-env && \
       export CUDF_HOME=${PWD} && \
       export CUDF_ROOT=${PWD}/cpp/build/ && \
       export CMAKE_LIBRARY_PATH=${CUDA_CUDA_LIBRARY} && \
-      ./build.sh libcudf cudf dask_cudf --allgpuarch && \
+      ./build.sh libcudf cudf dask_cudf --allgpuarch --cmake-args=\"-DCUDF_ENABLE_ARROW_S3=OFF\" && \
       protoc -I=python/cudf/cudf/utils/metadata --python_out=/usr/local/lib/python3.8/dist-packages/cudf/utils/metadata python/cudf/cudf/utils/metadata/orc_column_statistics.proto && \
     popd && \
     rm -rf build-env
@@ -131,39 +130,30 @@ FROM phase2 AS phase3
 
 ARG RELEASE=false
 ARG NVTAB_VER=vnightly
-
-RUN apt-get update -y && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-        vim gdb git wget unzip tar python3.8-dev \
-        zlib1g-dev lsb-release clang-format libboost-all-dev \
-        openssl curl zip\
-       	slapd && \
-    rm -rf /var/lib/apt/lists/*
+ARG TF4REC_VER=vnightly
 
 ENV PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION='python'
 
-
 RUN pip install pandas sklearn ortools nvtx-plugins pydot && \
     pip cache purge
-
-# install ucx from source
-RUN apt update; apt install -y libtool
-RUN git clone https://github.com/openucx/ucx.git /repos/ucx;cd /repos/ucx; ./autogen.sh; mkdir build; cd build; ../contrib/configure-release --prefix=/usr; make; make install
 
 RUN pip install pybind11
 SHELL ["/bin/bash", "-c"]
 
 # Install NVTabular
-RUN git clone https://github.com/NVIDIA/NVTabular.git /nvtabular/ && \
+RUN git clone https://github.com/NVIDIA-Merlin/NVTabular.git /nvtabular/ && \
     cd /nvtabular/; if [ "$RELEASE" == "true" ] && [ ${NVTAB_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${NVTAB_VER}; else git checkout main; fi; \
     python setup.py develop --user;
 
 # Install Transformers4Rec
-RUN pip install git+git://github.com/NVIDIA-Merlin/Transformers4Rec.git@v0.1.1#egg=transformers4rec[tensorflow,nvtabular]
+RUN git clone https://github.com/NVIDIA-Merlin/Transformers4Rec.git /transformers4rec && \
+    cd /transformers4rec/;  if [ "$RELEASE" == "true" ] && [ ${TF4REC_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${TF4REC_VER}; else git checkout main; fi; \
+    pip install -e .[tensorflow,nvtabular]
 
 RUN pip install pynvml pytest graphviz sklearn scipy matplotlib 
 RUN pip install nvidia-pyindex; pip install tritonclient[all] grpcio-channelz
-RUN pip install nvtx mpi4py cupy-cuda114 cachetools typing_extensions fastavro
+#RUN pip install nvtx mpi4py cupy-cuda114 cachetools typing_extensions fastavro
+RUN pip install nvtx cupy-cuda114 cachetools typing_extensions fastavro
 
 RUN apt-get update; apt-get install -y graphviz
 
@@ -177,22 +167,22 @@ RUN git clone https://github.com/rapidsai/asvdb.git build-env && \
     popd && \
     rm -rf build-env
 
-RUN pip install dask==2021.07.1 distributed==2021.07.1 dask[dataframe]==2021.07.1 dask-cuda
+RUN pip install dask==2021.09.1 distributed==2021.09.1 dask[dataframe]==2021.09.1 dask-cuda
 FROM phase3 as phase4
 
 ARG RELEASE=false
-ARG HUGECTR_VER=v21.9
+ARG HUGECTR_VER=vnightly
 ARG SM="60;61;70;75;80"
 ARG USE_NVTX=OFF
 
 RUN mkdir -p /usr/local/nvidia/lib64 && \
     ln -s /usr/local/cuda/lib64/libcusolver.so /usr/local/nvidia/lib64/libcusolver.so.10
 
-RUN ln -s /usr/lib/x86_64-linux-gnu/libibverbs.so.1.11.32.1 /usr/lib/x86_64-linux-gnu/libibverbs.so
+RUN ln -s /usr/lib/x86_64-linux-gnu/libibverbs.so.1 /usr/lib/x86_64-linux-gnu/libibverbs.so
 
-RUN git clone https://github.com/NVIDIA/HugeCTR.git build-env && \
+RUN git clone https://github.com/NVIDIA-Merlin/HugeCTR.git build-env && \
     pushd build-env && \
-      if [ "$RELEASE" == "true" ] && [ ${HUGECTR_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${HUGECTR_VER}; else echo ${HUGECTR_VER} && git checkout ${HUGECTR_VER}-integration; fi && \
+      if [ "$RELEASE" == "true" ] && [ ${HUGECTR_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${HUGECTR_VER}; else echo ${HUGECTR_VER} && git checkout master; fi && \
       cd sparse_operation_kit && \
       bash ./install.sh --SM=$SM --USE_NVTX=$USE_NVTX && \
     popd && \
@@ -201,6 +191,9 @@ RUN git clone https://github.com/NVIDIA/HugeCTR.git build-env && \
 
 RUN pip install pybind11
 RUN pip install numba numpy --upgrade
+
+RUN rm -rf /usr/local/share/jupyter/lab/staging/node_modules/fast-json-patch
+
 SHELL ["/bin/bash", "-c"]
 
 RUN echo $(du -h --max-depth=1 /)
