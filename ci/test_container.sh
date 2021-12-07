9f852bc698af26fa9b737e2ca7ba4e76a1fc7bc0
Alberto Alvarez <albertoa@nvidia.com>
2021-12-07 01:34:31 -0800

    Updates tests

--- ci/test_container.sh
+++ ci/test_container.sh
@@ -7,10 +7,11 @@ container=$1
 # Unit tests #
 ##############
 
-## Test NVTabular - All containers
+## Test NVTabular
 pytest /nvtabular/tests/unit
 
-## Test HugeCTR - Training container
+## Test HugeCTR
+### Training container
 if [ "$container" == "merlin-training" ]; then
     # layers_test && \ Running oom in blossom
     checker_test && \
@@ -21,19 +22,15 @@ if [ "$container" == "merlin-training" ]; then
     regularizers_test # && \
     # parser_test && \ Needs Multi-GPU
     # auc_test Needs Multi-GPU
-## Test Transformers4Rec - Tensorflow container
-elif [ "$container" == "merlin-tensorflow-training" ]; then
-    pytest /transformers4rec/tests/tf
-# Test Transformers4Rec - Pytorch container
-elif [ "$container" == "merlin-pytorch-training" ]; then
-    pytest /transformers4rec/tests/torch
-# Test HugeCTR & Transformers4Rec - Inference container
-elif [ "$container" == "merlin-inference" ]; then
+### Inference container
+# elif [ "$container" == "merlin-inference" ]; then
     # HugeCTR - Deactivated until it is self-contained and it runs
     # inference_test
-    # Transformers4Rec
-    pytest /transformers4rec/tests
-fi
+# fi
+
+## Test Transformers4Rec
+/transformers4rec/ci/test_unit.sh $container 0
+
 
 #####################
 # Integration tests #
