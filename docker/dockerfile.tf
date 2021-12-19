192ab236bdf63ad42c3dba27e4393ec16888289e
王泽寰 （Zehuan Wang） <zehuanw@nvidia.com>
2021-12-19 20:59:53 +0800

    Update dockerfile.tf

--- docker/dockerfile.tf
+++ docker/dockerfile.tf
@@ -94,20 +94,27 @@ ARG RELEASE=false
 ARG HUGECTR_VER=vnightly
 ARG SM="60;61;70;75;80"
 ARG USE_NVTX=OFF
+ARG INSTALL_HUGECTR=true
+# Arguments "_XXXX" are only valid when $DEV_MOD==ture
+ARG DEV_MODE=false
+ARG _HUGECTR_BRANCH=master
+ARG _HUGECTR_REPO="https://github.com/NVIDIA-Merlin/HugeCTR.git"
+ARG _CI_JOB_TOKEN=""
 
 RUN mkdir -p /usr/local/nvidia/lib64 && \
     ln -s /usr/local/cuda/lib64/libcusolver.so /usr/local/nvidia/lib64/libcusolver.so.10
 
 RUN ln -s /usr/lib/x86_64-linux-gnu/libibverbs.so.1 /usr/lib/x86_64-linux-gnu/libibverbs.so
 
-RUN git clone https://github.com/NVIDIA-Merlin/HugeCTR.git build-env && \
+RUN if [ "$INSTALL_HUGECTR" == "true" ]; then if [ "$DEV_MODE" == "true" ]; then git clone ${_CI_JOB_TOKEN}${_HUGECTR_REPO} build-env && pushd build-env && git fetch --all; if [ "$_HUGECTR_BRANC\
+H" != "" ]; then git checkout ${HUGECTR_BRANCH}; else git checkout master; fi; else git clone https://github.com/NVIDIA-Merlin/HugeCTR.git build-env && \
     pushd build-env && \
-      if [ "$RELEASE" == "true" ] && [ ${HUGECTR_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${HUGECTR_VER}; else echo ${HUGECTR_VER} && git checkout master; fi && \
+      if [ "$RELEASE" == "true" ] && [ ${HUGECTR_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${HUGECTR_VER}; else echo ${HUGECTR_VER} && git checkout master; fi ; fi && \
       cd sparse_operation_kit && \
       python setup.py install && \
     popd && \
     rm -rf build-env && \
-    rm -rf /var/tmp/HugeCTR
+    rm -rf /var/tmp/HugeCTR; fi;
 
 RUN pip install pybind11
 RUN pip install numba numpy --upgrade
