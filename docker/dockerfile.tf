948d8a01d460ea1d5963340d1f2463405eb36b87
Alberto Alvarez <albertoa@nvidia.com>
2022-02-23 10:45:01 -0800

    Reduce containers size (#100)
    * Mounts rapids
    
    * Update install
    
    * Cleans dockerfile torch
    
    * Reducing install
    
    * Updates inference
    
    * Updates
    
    * Updates inference deps
    
    * Adds graphvuz
    
    * Adds tritonclient in training
    
    * Adds pip graphviz
    
    * Reinstalls pyarrow
    
    * Updates
    
    * Installs NVTabular properly
    
    * Fixes typo
    
    * Add pybind11
    
    * No deps rmm
    
    * Add protobuf
    
    * Updates dockerfiles
    
    * Fixes typo
    
    * Fixes nvt install
    
    * removes -e
    
    * Inference works
    
    * Forces numpy
    
    * Cleanup
    
    * Add core
    
    * removes deps
    
    * Fixes installs
    
    * Adds numba
    
    * Removes Numba
    
    * Latests numba
    
    * Updates numba
    
    * Add python ENV
    
    * Numpy version
    
    * install deps
    
    * Update llvmlite
    
    * Don't report
    
    * llvlite update
    
    * Updates torch
    
    * Upgrade setup tools
    
    * Update required versions

--- docker/dockerfile.tf
+++ docker/dockerfile.tf
@@ -3,11 +3,11 @@ ARG IMAGE=nvcr.io/nvidia/tensorflow:22.01-tf2-py3
 FROM ${IMAGE}
 
 # Args
-ARG RELEASE=false
-ARG NVTAB_VER=vnightly
-ARG MODELS_VER=vnightly
-ARG HUGECTR_VER=vnightly
-ARG TF4REC_VER=vnightly
+ARG CORE_VER=main
+ARG HUGECTR_VER=master
+ARG NVTAB_VER=main
+ARG MODELS_VER=main
+ARG TF4REC_VER=main
 
 # Envs
 ENV LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/lib:/repos/dist/lib
@@ -16,79 +16,57 @@ ENV CUDA_PATH=$CUDA_HOME
 ENV CUDA_CUDA_LIBRARY=${CUDA_HOME}/lib64/stubs
 ENV PATH=${CUDA_HOME}/lib64/:${PATH}:${CUDA_HOME}/bin
 
-# Install packages
+# Install system packages
 ENV DEBIAN_FRONTEND=noninteractive
 RUN apt update -y --fix-missing && \
-    apt upgrade -y && \
     apt install -y --no-install-recommends software-properties-common && \
-    add-apt-repository -y ppa:deadsnakes/ppa && \
-    apt update -y --fix-missing && \
     apt-get install -y --no-install-recommends \
-        gdb \
-        valgrind \
-        zlib1g-dev lsb-release clang-format libboost-serialization-dev \
-        openssl \
         graphviz \
-        libssl-dev \
-        protobuf-compiler \
-        libaio-dev \
-        slapd && \
+        protobuf-compiler && \
     apt-get autoremove -y && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
-# Install cmake
-RUN apt remove --purge cmake -y && wget http://www.cmake.org/files/v3.21/cmake-3.21.1.tar.gz && \
-    tar xf cmake-3.21.1.tar.gz && cd cmake-3.21.1 && ./configure && make && make install
-
 # Install multiple packages
-RUN pip cache purge
-RUN pip install nvtx pandas cupy-cuda115 cachetools typing_extensions fastavro
-RUN pip install pynvml pytest graphviz scipy matplotlib tqdm pydot nvidia-pyindex
+RUN pip install betterproto graphviz pybind11 pydot pytest
+RUN pip install nvidia-pyindex
 RUN pip install tritonclient[all] grpcio-channelz
-RUN pip install pybind11 jupyterlab gcsfs
-RUN pip3 install --no-cache-dir mpi4py ortools sklearn onnx onnxruntime
-RUN pip install dask==2021.11.2 distributed==2021.11.2 dask[dataframe]==2021.11.2 dask-cuda
-RUN pip install betterproto tensorflow-metadata
-RUN pip install gevent==21.8.0
-RUN pip install --no-cache-dir git+https://github.com/rapidsai/asvdb.git@main
+RUN pip install numba==0.55.1
+RUN pip install git+https://github.com/rapidsai/asvdb.git@main
+
+# Install Merlin Core
+RUN git clone https://github.com/NVIDIA-Merlin/core.git /core/ && \
+    cd /core/ && git checkout ${CORE_VER} && pip install --no-deps -e .
+ENV PYTHONPATH=/core:$PYTHONPATH
 
 ARG INSTALL_NVT=true
 # Install NVTabular
 ENV PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION='python'
 RUN if [ "$INSTALL_NVT" == "true" ]; then \
-        git clone https://github.com/NVIDIA-Merlin/NVTabular.git /nvtabular/ && \
-        cd /nvtabular/; if [ "$RELEASE" == "true" ] && [ ${NVTAB_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${NVTAB_VER}; else git checkout main; fi; \
-        python setup.py develop --no-deps; \
+      git clone https://github.com/NVIDIA-Merlin/NVTabular.git /nvtabular/ && \
+      cd /nvtabular/ && git checkout ${NVTAB_VER} && pip install --no-deps -e .; \
     fi
+ENV PYTHONPATH=/nvtabular:$PYTHONPATH
 
 # Install Transformers4Rec
-RUN pip install transformers
 RUN if [ "$INSTALL_NVT" == "true" ]; then \
-        git clone https://github.com/NVIDIA-Merlin/Transformers4Rec.git /transformers4rec && \
-        cd /transformers4rec/;  if [ "$RELEASE" == "true" ] && [ ${TF4REC_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${TF4REC_VER}; else git checkout main; fi; \
-        pip install -e .[tensorflow,nvtabular] --no-deps && python setup.py develop --no-deps; \
+      git clone https://github.com/NVIDIA-Merlin/Transformers4Rec.git /transformers4rec && \
+      cd /transformers4rec/ && git checkout ${TF4REC_VER} && pip install . --no-deps; \
     fi
+ENV PYTHONPATH=/transformers4rec:$PYTHONPATH
 
 # Install Models
 RUN git clone https://github.com/NVIDIA-Merlin/Models.git /models/ && \
-    cd /models/; if [ "$RELEASE" == "true" ] && [ ${MODELS_VER} != "vnightly" ] ; then git fetch --all --tags && git checkout tags/${MODELS_VER}; else git checkout main; fi; \
-    python setup.py develop --no-deps;
+    cd /models/ && git checkout ${MODELS_VER} && pip install -e . --no-deps
+ENV PYTHONPATH=/models:$PYTHONPATH
 
 # Install HugeCTR
 ENV LD_LIBRARY_PATH=/usr/local/hugectr/lib:$LD_LIBRARY_PATH \
     LIBRARY_PATH=/usr/local/hugectr/lib:$LIBRARY_PATH \
     PYTHONPATH=/usr/local/hugectr/lib:$PYTHONPATH
 
-RUN git clone https://github.com/rapidsai/asvdb.git build-env && \
-    pushd build-env && \
-      python setup.py install && \
-    popd && \
-    rm -rf build-env
-
 # Arguments "_XXXX" are only valid when $HUGECTR_DEV_MODE==false
 ARG HUGECTR_DEV_MODE=false
-ARG _HUGECTR_BRANCH=master
 ARG _HUGECTR_REPO="github.com/NVIDIA-Merlin/HugeCTR.git"
 ARG _CI_JOB_TOKEN=""
 
@@ -98,23 +76,17 @@ RUN mkdir -p /usr/local/nvidia/lib64 && \
 RUN ln -s /usr/lib/x86_64-linux-gnu/libibverbs.so.1 /usr/lib/x86_64-linux-gnu/libibverbs.so
 
 RUN if [ "$HUGECTR_DEV_MODE" == "false" ]; then \
-        git clone https://${_CI_JOB_TOKEN}${_HUGECTR_REPO} build-env && pushd build-env && git fetch --all; \
-        if [ "$RELEASE" == "true" ] && [ ${HUGECTR_VER} != "vnightly" ]; then \
-            git fetch --all --tags && git checkout tags/${HUGECTR_VER}; \
-        else \
-            git checkout ${_HUGECTR_BRANCH}; \
-        fi; \
-        cd sparse_operation_kit && \
-        python setup.py install && \
+        git clone https://${_CI_JOB_TOKEN}${_HUGECTR_REPO} build-env && \
+        pushd build-env && \ 
+          git checkout ${HUGECTR_VER} && \
+          cd sparse_operation_kit && \
+          python setup.py install && \
         popd && \
         rm -rf build-env; \
     fi
 
 # Clean up
 RUN rm -rf /repos
-RUN rm -rf /usr/local/share/jupyter/lab/staging/node_modules/fast-json-patch
-
-RUN echo $(du -h --max-depth=1 /)
 
 HEALTHCHECK NONE
 CMD ["/bin/bash"]
