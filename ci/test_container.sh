a6a17ed7e1adcfa50acb2d5bfb1282a8aad0add5
Alberto Alvarez <albertoa@nvidia.com>
2022-04-05 15:53:19 -0700

    Merlin Container Release 22.04 (#178)
    * Update base
    
    * Test container update
    
    * Upgrade
    
    * Fixes rmm bug
    
    * Fix typo
    
    * Remove Merlin suite from CI container
    
    * Fixes Merlin install
    
    * Add libarchive
    
    * tmp
    
    * Add pydot
    
    * Add Merlin Repo

--- ci/test_container.sh
+++ ci/test_container.sh
@@ -3,38 +3,72 @@
 container=$1
 devices=$2
 
-##############
-# Unit tests #
-##############
+
+
+#print("##################")
+#print("# Software check #")
+#print("##################")
+
+regex="merlin(.)*-inference"
+if [[ ! "$container" =~ $regex ]]; then
+#    print("Check tritonserver for inference containers")
+    whereis tritonserver
+fi
+
+if [ "$container" == "merlin-training" ]; then
+#    print("Check HugeCTR for ctr-training container")
+    python -c "import hugectr; print(hugectr.__version.__)"
+fi
+
+if [ "$container" == "merlin-tensorflow-training" ]; then
+#    print("Check TensorFlow for tf-training container")
+    python -c "import tensorflow; print(tensorflow.__version__)"
+fi
+
+if [ "$container" == "merlin-pytorch-training" ]; then
+#    print("Check PyTorch for torch-training container")
+    python -c "import torch; print(torch.__version__)"
+fi
+
+#print("##############")
+#print("# Unit tests #")
+#print("##############")
 
 ## Test Core
+#print("Run unit tests for Core")
 /core/ci/test_unit.sh $container $devices
 
 ## Test NVTabular
+#print("Run unit tests for NVTabular")
 /nvtabular/ci/test_unit.sh $container $devices
 
 ## Test Transformers4Rec
+#print("Run unit tests for Transformers4Rec")
 /transformers4rec/ci/test_unit.sh $container $devices
 
 ## Test Models
+#print("Run unit tests for Models")
 pip install coverage
 /models/ci/test_unit.sh $container $devices
 
 ## Test HugeCTR
 if [ "$container" == "merlin-training" ]; then
+#    print("Run unit tests for HugeCTR")
     /hugectr/ci/test_unit.sh $container $devices
 fi
 
-#####################
-# Integration tests #
-#####################
+#print("#####################")
+#print("# Integration tests #")
+#print("#####################")
 
 # Test NVTabular 
-## Not shared storage in blossom yet
+## Not shared storage in blossom yet, inference testing cannot be run
 regex="merlin(.)*-inference"
 if [[ ! "$container" =~ $regex ]]; then
+#    print("Run instegration tests for NVTabular")
     /nvtabular/ci/test_integration.sh $container $devices --report 1
 fi
 
 # Test Transformers4Rec
+#print("Run integration tests for Transformers4Rec")
 /transformers4rec/ci/test_integration.sh $container $devices
--- docker/training/dockerfile.tf
+++ docker/training/dockerfile.tf
@@ -1,5 +1,5 @@
 # syntax=docker/dockerfile:1
-ARG IMAGE=nvcr.io/nvidia/tensorflow:22.02-tf2-py3
+ARG IMAGE=nvcr.io/nvidia/tensorflow:22.03-tf2-py3
 FROM ${IMAGE}
 
 # Args
@@ -35,6 +35,7 @@ RUN apt update -y --fix-missing && \
 # Install multiple packages
 RUN pip install betterproto graphviz pybind11 pydot pytest mpi4py transformers==4.12
 RUN pip install --upgrade ipython
+RUN pip install --upgrade horovod
 RUN pip install nvidia-pyindex
 RUN pip install tritonclient[all] grpcio-channelz
 RUN pip install numba==0.55.1
@@ -43,12 +44,12 @@ RUN pip install git+https://github.com/rapidsai/asvdb.git@main
 # Install Merlin Core
 RUN git clone https://github.com/NVIDIA-Merlin/core.git /core/ && \
     cd /core/ && git checkout ${CORE_VER} && pip install --no-deps -e .
-ENV PYTHONPATH=/core:$PYTHONPATH
+ENV PYTHONPATH=$PYTHONPATH:/core
 
 # Install Merlin Systems
 RUN git clone https://github.com/NVIDIA-Merlin/systems.git /systems/ && \
     cd /systems/ && git checkout ${SYSTEMS_VER} && pip install --no-deps -e .
-    ENV PYTHONPATH=/systems:$PYTHONPATH
+    ENV PYTHONPATH=$PYTHONPATH:/systems
 
 ARG INSTALL_NVT=true
 # Install NVTabular
@@ -57,19 +58,22 @@ RUN if [ "$INSTALL_NVT" == "true" ]; then \
       git clone https://github.com/NVIDIA-Merlin/NVTabular.git /nvtabular/ && \
       cd /nvtabular/ && git checkout ${NVTAB_VER} && pip install --no-deps -e .; \
     fi
-ENV PYTHONPATH=/nvtabular:$PYTHONPATH
+ENV PYTHONPATH=$PYTHONPATH:/nvtabular
 
 # Install Transformers4Rec
 RUN if [ "$INSTALL_NVT" == "true" ]; then \
       git clone https://github.com/NVIDIA-Merlin/Transformers4Rec.git /transformers4rec && \
       cd /transformers4rec/ && git checkout ${TF4REC_VER} && pip install . --no-deps; \
     fi
-ENV PYTHONPATH=/transformers4rec:$PYTHONPATH
+ENV PYTHONPATH=$PYTHONPATH:/transformers4rec
 
 # Install Models
 RUN git clone https://github.com/NVIDIA-Merlin/Models.git /models/ && \
     cd /models/ && git checkout ${MODELS_VER} && pip install -e . --no-deps
-ENV PYTHONPATH=/models:$PYTHONPATH
+ENV PYTHONPATH=$PYTHONPATH:/models
+
+# Add Merlin Repo
+RUN git clone https://github.com/NVIDIA-Merlin/Merlin/ /Merlin
 
 # Install HugeCTR
 ENV LD_LIBRARY_PATH=/usr/local/hugectr/lib:$LD_LIBRARY_PATH \
