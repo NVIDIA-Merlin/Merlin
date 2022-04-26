c2c429faa633cf6b1ae95b4935cc31f3cb80234c
Karl Higley <kmhigley@gmail.com>
2022-04-26 15:48:06 -0400

    Restructure container builds to use multi-stage builds and a Merlin base image (#234)
    * working on all in one
    
    * Add DLFW image as source in TF Dockerfile
    
    * Update Merlin base image dockerfile to adjust multi-stage build
    
    * Set `DEBIAN_FRONTEND=noninteractive` earlier
    
    * Remove repeated Triton back-end copies
    
    * Add Python 3 system packages for pip and dev to final container
    
    * Skip copying `site_packages` (since it doesn't exist)
    
    * Fix `testbooks` to `testbook`
    
    * Add missing system packages
    
    * Fix up the TF image to build on top of Merlin base image
    
    * Avoid wasting space on multiple copies of Tensorflow package files
    
    * Base the Torch container on the Merlin base image
    
    * Add `container-structure-test` test definitions for Merlin/TF/Torch
    
    * Clean up stray set up args from `dockerfile.merlin`
    
    * Add package import tests to base container
    
    * Fix and clean up Torch dockerfile
    
    * Expand and refine the TF container tests
    
    * Clean up dockerfile.torch
    
    * Temporarily disable Merlin Models tests for TF container
    
    * Move dockerfiles and tests to the `docker` directory
    
    * Use `--no-cache-dir` for `pip install`
    
    * Avoid copying shared libraries from DLFW Torch container
    
    We just needed to install a few system packages
    
    * Re-build the HugeCTR container on top of the Merlin base image
    
    * Use `base` instead of `build` in TF container
    
    * Add RMM includes to the Merlin base container
    
    * Switch to installing `jupyterlab` instead of `jupyter` in base container
    
    * Re-enable the Merlin Models tests in the TF container
    
    * Add a container test for HugeCTR
    
    Co-authored-by: Julio Perez <jperez@nvidia.com>

---
+++ docker/dockerfile.tf
@@ -0,0 +1,28 @@
+# syntax=docker/dockerfile:1.2
+ARG MERLIN_VERSION=22.05-pre
+ARG TRITON_VERSION=22.03
+ARG TENSORFLOW_VERSION=22.03
+
+ARG DLFW_IMAGE=nvcr.io/nvidia/tensorflow:${TENSORFLOW_VERSION}-tf2-py3
+ARG FULL_IMAGE=nvcr.io/nvidia/tritonserver:${TRITON_VERSION}-py3
+ARG BASE_IMAGE=merlin/base:${MERLIN_VERSION}
+
+FROM ${DLFW_IMAGE} as dlfw
+FROM ${FULL_IMAGE} as triton
+FROM ${BASE_IMAGE} as base
+
+# Triton TF backends
+COPY --chown=1000:1000 --from=triton /opt/tritonserver/backends/tensorflow2 backends/tensorflow2/
+
+# Tensorflow dependencies (only)
+RUN pip install tensorflow-gpu \
+    && rm -r /usr/local/lib/python3.8/dist-packages/tensorflow
+
+# DLFW Tensorflow packages
+COPY --chown=1000:1000 --from=dlfw /usr/local/lib/python3.8/dist-packages/tensorflow /usr/local/lib/python3.8/dist-packages/tensorflow/
+COPY --chown=1000:1000 --from=dlfw /usr/local/lib/tensorflow/ /usr/local/lib/tensorflow/
+COPY --chown=1000:1000 --from=dlfw /usr/local/lib/python3.8/dist-packages/horovod /usr/local/lib/python3.8/dist-packages/horovod/
+COPY --chown=1000:1000 --from=dlfw /usr/local/bin/horovodrun /usr/local/bin/horovodrun
+
+HEALTHCHECK NONE
+CMD ["/bin/bash"]
