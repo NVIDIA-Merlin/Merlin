7228754f6ba4febc4bc610f37a668c64df7e6104
rnyak <ronayak@hotmail.com>
2022-05-04 16:08:19 -0400

    Delete tmp_cell-checkpoint.py

--- tests/examples/.ipynb_checkpoints/tmp_cell-checkpoint.py
+++
@@ -1,3 +0,0 @@
-import json
-document = json.load(open('/Merlin/examples/Building-and-deploying-multi-stage-RecSys/02-Deploying-multi-stage-RecSys-with-Merlin-Systems.ipynb','r'))
-print(len(document['cells'])
