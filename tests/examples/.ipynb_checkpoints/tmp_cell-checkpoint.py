ccac466318cc8dcb4d56affad8b0e9cdceb3ca4f
root <ronayak@hotmail.com>
2022-05-04 20:05:21 +0000

    add unit test for poc_nbs

---
+++ tests/examples/.ipynb_checkpoints/tmp_cell-checkpoint.py
@@ -0,0 +1,3 @@
+import json
+document = json.load(open('/Merlin/examples/Building-and-deploying-multi-stage-RecSys/02-Deploying-multi-stage-RecSys-with-Merlin-Systems.ipynb','r'))
+print(len(document['cells'])
