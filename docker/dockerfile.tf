96576b647ff86ac5724924840af1e75b9bff67bf
Julio Perez <jperez@nvidia.com>
2022-05-26 23:03:45 -0400

    add keras to pull from upstream container

--- docker/dockerfile.tf
+++ docker/dockerfile.tf
@@ -1,11 +1,11 @@
 # syntax=docker/dockerfile:1.2
-ARG MERLIN_VERSION=22.05-pre
-ARG TRITON_VERSION=22.03
-ARG TENSORFLOW_VERSION=22.03
+ARG MERLIN_VERSION=22.06
+ARG TRITON_VERSION=22.05
+ARG TENSORFLOW_VERSION=22.05
 
 ARG DLFW_IMAGE=nvcr.io/nvidia/tensorflow:${TENSORFLOW_VERSION}-tf2-py3
 ARG FULL_IMAGE=nvcr.io/nvidia/tritonserver:${TRITON_VERSION}-py3
-ARG BASE_IMAGE=merlin/base:${MERLIN_VERSION}
+ARG BASE_IMAGE=nvcr.io/nvstaging/merlin/merlin-base:${MERLIN_VERSION}
 
 FROM ${DLFW_IMAGE} as dlfw
 FROM ${FULL_IMAGE} as triton
@@ -20,9 +20,11 @@ RUN pip install tensorflow-gpu \
 
 # DLFW Tensorflow packages
 COPY --chown=1000:1000 --from=dlfw /usr/local/lib/python3.8/dist-packages/tensorflow /usr/local/lib/python3.8/dist-packages/tensorflow/
+COPY --chown=1000:1000 --from=dlfw /usr/local/lib/python3.8/dist-packages/keras /usr/local/lib/python3.8/dist-packages/keras/
 COPY --chown=1000:1000 --from=dlfw /usr/local/lib/tensorflow/ /usr/local/lib/tensorflow/
 COPY --chown=1000:1000 --from=dlfw /usr/local/lib/python3.8/dist-packages/horovod /usr/local/lib/python3.8/dist-packages/horovod/
 COPY --chown=1000:1000 --from=dlfw /usr/local/bin/horovodrun /usr/local/bin/horovodrun
+
 
 HEALTHCHECK NONE
 CMD ["/bin/bash"]
