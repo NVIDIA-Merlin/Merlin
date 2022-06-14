9503f21185600f70208a1d93235b63d7c2e1e65b
Julio Perez <jperez@nvidia.com>
2022-06-14 11:56:10 -0400

    fix test harness, add exists check to file removes and comment out pip install cell

--- tests/unit/examples/test_building_deploying_multi_stage_RecSys.py
+++ tests/unit/examples/test_building_deploying_multi_stage_RecSys.py
@@ -5,32 +5,36 @@ from testbook import testbook
 from tests.conftest import REPO_ROOT
 
 
-@testbook(
-    REPO_ROOT
-    / "examples/Building-and-deploying-multi-stage-RecSys/01-Building-Recommender-Systems-with-Merlin.ipynb",
-    execute=False,
-)
-def test_func(tb1):
-    tb1.inject(
-        """
-        import os
-        os.environ["DATA_FOLDER"] = "/tmp/data/"
-        os.environ["NUM_ROWS"] = "10000"
-        os.system("mkdir -p /tmp/examples")
-        os.environ["BASE_DIR"] = "/tmp/examples/"
-        """
-    )
-    tb1.execute()
-    assert os.path.isdir("/tmp/examples/dlrm")
-    assert os.path.isdir("/tmp/examples/feature_repo")
-    assert os.path.isdir("/tmp/examples/query_tower")
-    assert os.path.isfile("/tmp/examples/item_embeddings.parquet")
-    assert os.path.isfile("/tmp/examples/feature_repo/user_features.py")
-    assert os.path.isfile("/tmp/examples/feature_repo/item_features.py")
+def test_func():
+    with testbook(
+        REPO_ROOT
+        / "examples"
+        / "Building-and-deploying-multi-stage-RecSys"
+        / "01-Building-Recommender-Systems-with-Merlin.ipynb",
+        execute=False,
+    ) as tb1:
+        tb1.inject(
+            """
+            import os
+            os.environ["DATA_FOLDER"] = "/tmp/data/"
+            os.environ["NUM_ROWS"] = "10000"
+            os.system("mkdir -p /tmp/examples")
+            os.environ["BASE_DIR"] = "/tmp/examples/"
+            """
+        )
+        tb1.execute()
+        assert os.path.isdir("/tmp/examples/dlrm")
+        assert os.path.isdir("/tmp/examples/feature_repo")
+        assert os.path.isdir("/tmp/examples/query_tower")
+        assert os.path.isfile("/tmp/examples/item_embeddings.parquet")
+        assert os.path.isfile("/tmp/examples/feature_repo/user_features.py")
+        assert os.path.isfile("/tmp/examples/feature_repo/item_features.py")
 
     with testbook(
         REPO_ROOT
-        / "examples/Building-and-deploying-multi-stage-RecSys/02-Deploying-multi-stage-RecSys-with-Merlin-Systems.ipynb",
+        / "examples"
+        / "Building-and-deploying-multi-stage-RecSys"
+        / "02-Deploying-multi-stage-RecSys-with-Merlin-Systems.ipynb",
         execute=False,
     ) as tb2:
         tb2.inject(
@@ -44,7 +48,6 @@ def test_func(tb1):
         tb2.execute_cell(list(range(0, NUM_OF_CELLS - 3)))
         top_k = tb2.ref("top_k")
         outputs = tb2.ref("outputs")
-        request = tb2.ref("request")
         assert outputs[0] == "ordered_ids"
         tb2.inject(
             """
