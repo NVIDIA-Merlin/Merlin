eaf8f2b90713c82ce84c13fafe06c1e231ad3429
Gabriel Moreira <gmoreira@nvidia.com>
2022-07-06 22:05:52 -0300

    Added integration tests to Merlin Models

--- ci/test_container.sh
+++ ci/test_container.sh
@@ -12,6 +12,25 @@ else
     ci_script_dir=./
 fi
 
+<<<<<<< HEAD
 ${ci_script_dir}software_check.sh $container $devices
 ${ci_script_dir}unit_tests.sh $container $devices
 ${ci_script_dir}integration_tests.sh $container $devices $suppress_failures
+=======
+echo "#####################"
+echo "# Integration tests #"
+echo "#####################"
+
+# Test NVTabular 
+## Not shared storage in blossom yet, inference testing cannot be run
+echo "Run integration tests for NVTabular"
+/nvtabular/ci/test_integration.sh $container $devices --report 1
+
+# Test Transformers4Rec
+echo "Run integration tests for Merlin Models"
+/models/ci/test_integration.sh $container $devices
+
+# Test Transformers4Rec
+echo "Run integration tests for Transformers4Rec"
+/transformers4rec/ci/test_integration.sh $container $devices
+>>>>>>> Added integration tests to Merlin Models
