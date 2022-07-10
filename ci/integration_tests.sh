000d8b2241d900981af97891c5d86d9eefc9fa62
Karl Higley <kmhigley@gmail.com>
2022-07-10 18:00:39 -0400

    Make the scripts run all checks and return overall error code
    This allows all software checks, all unit tests, or all integration tests (depending on the sub-script) to run and potentially fail before returning an overall error code, which reflects the failure of any command in that particular script.

--- ci/integration_tests.sh
+++ ci/integration_tests.sh
@@ -7,11 +7,15 @@ echo "#####################"
 echo "# Integration tests #"
 echo "#####################"
 
+exit_code=0
+
 # Test NVTabular 
 ## Not shared storage in blossom yet, inference testing cannot be run
 echo "Run integration tests for NVTabular"
-/nvtabular/ci/test_integration.sh $container $devices --report 1
+/nvtabular/ci/test_integration.sh $container $devices --report 1 || exit_code=1
 
 # Test Transformers4Rec
 echo "Run integration tests for Transformers4Rec"
-/transformers4rec/ci/test_integration.sh $container $devices
+/transformers4rec/ci/test_integration.sh $container $devices || exit_code=1
+
+exit $exit_code
