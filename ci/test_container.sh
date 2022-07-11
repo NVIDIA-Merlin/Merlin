4e825aecb2c40fa273e0c83d2a3d9d2f84067a4a
Karl Higley <kmhigley@gmail.com>
2022-07-10 23:08:09 -0400

    Refactor the container test script to run all the SW checks, unit tests, or integration tests before failing (#444)
    * Split container test script into software, unit, and integration tests
    
    * Make the scripts run all checks and return overall error code
    
    This allows all software checks, all unit tests, or all integration tests (depending on the sub-script) to run and potentially fail before returning an overall error code, which reflects the failure of any command in that particular script.
    
    * Check presence of Triton server on all containers
    
    * Add an argument to suppress integration test failures
    
    * Add newlines at the end of files

--- ci/test_container.sh
+++ ci/test_container.sh
@@ -3,104 +3,15 @@ set -e
 
 container=$1
 devices=$2
+suppress_failures="${3:-1}"
 
-
-
-echo "##################"
-echo "# Software check #"
-echo "##################"
-
-regex="merlin(.)*-inference"
-if [[ ! "$container" =~ $regex ]]; then
-    echo "Check tritonserver for inference containers"
-    which tritonserver
+# Use the dirname directly, without changing directories
+if [[ $BASH_SOURCE = */* ]]; then
+    ci_script_dir=${BASH_SOURCE%/*}/
+else
+    ci_script_dir=./
 fi
 
-if [ "$container" == "merlin-hugectr" ]; then
-    echo "Check HugeCTR for ctr-training container"
-    python -c "import hugectr; print(hugectr.__version__)"
-
-    # TODO: remove this block once
-    # https://github.com/NVIDIA-Merlin/HugeCTR/pull/328
-    # is in the hugectr release
-    cd /hugectr && \
-    checker_test && \
-    device_map_test && \
-    loss_test && \
-    optimizer_test && \
-    regularizers_test
-fi
-
-if [ "$container" == "merlin-tensorflow" ]; then
-    echo "Check TensorFlow for merlin-tensorflow container"
-    python -c "import tensorflow; print(tensorflow.__version__)"
-    echo "Check merlin-sok for tf-training container"
-    python -c "import sparse_operation_kit; print(sparse_operation_kit.__version__)"
-    echo "Check distributed-embeddings for tf-training container"
-    python -c "import distributed_embeddings as tfde; print(tfde.__doc__)"
-
-    # TODO: remove this block once
-    # https://github.com/NVIDIA-Merlin/HugeCTR/pull/328
-    # is in the hugectr release
-    pushd /hugectr/sparse_operation_kit/unit_test/test_scripts/tf2 && \
-    bash sok_test_unit.sh && \
-    popd
-fi
-
-if [ "$container" == "merlin-pytorch" ]; then
-    echo "Check PyTorch for torch-training container"
-    python -c "import torch; print(torch.__version__)"
-fi
-
-echo "##############"
-echo "# Unit tests #"
-echo "##############"
-
-## Test Core
-echo "Run unit tests for Core"
-cd /core && ci/test_unit.sh $container $devices
-
-## Test NVTabular
-echo "Run unit tests for NVTabular"
-cd /nvtabular && ci/test_unit.sh $container $devices
-
-## Test Transformers4Rec
-echo "Run unit tests for Transformers4Rec"
-cd /transformers4rec/ && ci/test_unit.sh $container $devices
-
-## Test Models
-echo "Run unit tests for Models"
-pip install coverage
-cd /models/ && ci/test_unit.sh $container $devices
-
-## Test Systems
-echo "Run unit tests for Systems"
-cd /systems && pytest -rxs tests/unit
-
-## Test HugeCTR
-if [ "$container" == "merlin-hugectr" ]; then
-    echo "Run unit tests for HugeCTR"
-    /hugectr/ci/test_unit.sh $container $devices
-fi
-
-## Test distributed-embeddings
-if [ "$container" == "merlin-tensorflow" ]; then
-    echo "Run unit tests for merlin-sok"
-    /hugectr/ci/test_unit.sh $container $devices
-
-    echo "Run unit tests for distributed-embeddings"
-    pytest -rxs /distributed_embeddings/tests
-fi
-
-echo "#####################"
-echo "# Integration tests #"
-echo "#####################"
-
-# Test NVTabular 
-## Not shared storage in blossom yet, inference testing cannot be run
-echo "Run integration tests for NVTabular"
-/nvtabular/ci/test_integration.sh $container $devices --report 1
-
-# Test Transformers4Rec
-echo "Run integration tests for Transformers4Rec"
-/transformers4rec/ci/test_integration.sh $container $devices
+${ci_script_dir}software_check.sh $container $devices
+${ci_script_dir}unit-tests.sh $container $devices
+${ci_script_dir}integration_tests.sh $container $devices $suppress_failures
