d8c8d49af6df2f4eb8483f9e9df846fb0d017737
Julio Perez <37191411+jperez999@users.noreply.github.com>
2022-09-07 23:59:46 -0400

    anchor Tf version (#601)
    * merge resolved
    
    * anchoring tf version to 2.9.2
    
    * adding comment about why pinning occurred

--- docker/dockerfile.tf
+++ docker/dockerfile.tf
@@ -15,7 +15,8 @@ FROM ${BASE_IMAGE} as base
 COPY --chown=1000:1000 --from=triton /opt/tritonserver/backends/tensorflow2 backends/tensorflow2/
 
 # Tensorflow dependencies (only)
-RUN pip install tensorflow-gpu \
+# Pinning to pass hugectr sok tests
+RUN pip install tensorflow-gpu==2.9.2 \
     && pip uninstall tensorflow-gpu keras -y
 
 # DLFW Tensorflow packages
