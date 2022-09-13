f3700c2143c6c786849c42b761ef1b83359e0d75
root <ronayak@hotmail.com>
2022-09-13 20:58:57 +0000

    modify nb and unit test  wrt systems changes

--- tests/unit/examples/test_building_deploying_multi_stage_RecSys.py
+++ tests/unit/examples/test_building_deploying_multi_stage_RecSys.py
@@ -1,24 +1,21 @@
 import os
 
-from testbook import testbook
-
-from tests.conftest import REPO_ROOT
-from merlin.core.dispatch import get_lib
-from merlin.systems.triton.utils import run_ensemble_on_tritonserver
-
+import numpy as np
 import pytest
+from merlin.core.dispatch import get_lib
+from merlin.models.loader.tf_utils import configure_tensorflow
+from merlin.systems.triton.utils import run_ensemble_on_tritonserver
+from nvtabular import ColumnSchema, Schema
+from testbook import testbook
+from tests.conftest import REPO_ROOT
 
 pytest.importorskip("tensorflow")
 pytest.importorskip("feast")
 pytest.importorskip("faiss")
-from merlin.models.loader.tf_utils import configure_tensorflow
 
 # flake8: noqa
 
 
-@pytest.mark.skip(
-    "'NoneType' returning from faiss operator because of randomly generated data."
-)
 def test_func():
     with testbook(
         REPO_ROOT
@@ -64,18 +61,29 @@ def test_func():
         outputs = tb2.ref("outputs")
         assert outputs[0] == "ordered_ids"
 
+        request_schema = Schema(
+            [
+                ColumnSchema("user_id_raw", dtype=np.int32),
+            ]
+        )
         df_lib = get_lib()
 
-        # read in data for request
-        batch = df_lib.read_parquet(
-            os.path.join("/tmp/data/processed/retrieval/", "train", "part_0.parquet"),
+        # create a request to sent to TIS
+        request_df = df_lib.read_parquet(
+            os.path.join("/tmp/data/", "train_raw.parquet"),
             num_rows=1,
             columns=["user_id"],
         )
+        request_df = request_df.rename(columns={"user_id": "user_id_raw"})
+
         configure_tensorflow()
 
         response = run_ensemble_on_tritonserver(
-            "/tmp/examples/poc_ensemble/", outputs, batch, "ensemble_model"
+            "/tmp/examples/poc_ensemble/",
+            request_schema,
+            request_df,
+            outputs,
+            "ensemble_model",
         )
         response = response["ordered_ids"]
 
