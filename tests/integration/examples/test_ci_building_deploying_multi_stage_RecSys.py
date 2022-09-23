e6d6efa348fa0498bd7321e45f887e440176cf78
root <ronayak@hotmail.com>
2022-09-23 14:46:19 +0000

    fix import seedir error

--- tests/integration/examples/test_ci_building_deploying_multi_stage_RecSys.py
+++ tests/integration/examples/test_ci_building_deploying_multi_stage_RecSys.py
@@ -18,6 +18,7 @@ def test_func():
         execute=False,
         timeout=450,
     ) as tb1:
+        NUM_OF_CELLS = len(tb1.cells)
         tb1.inject(
             """
             import os
@@ -42,7 +43,7 @@ def test_func():
                 )
             """
         )
-        tb1.execute_cell(list(range(28, 79)))
+        tb1.execute_cell(list(range(28, NUM_OF_CELLS)))
         assert os.path.isdir("/tmp/examples/query_tower")
         assert os.path.isdir("/tmp/examples/dlrm")
         assert os.path.isdir("/tmp/examples/feature_repo")
