6f1dc2b5568e98bb152234dcd968f4db8913d276
Deyu Fu <deyuf@nvidia.com>
2022-10-09 19:55:25 +0800

    update TFDE build

--- docker/dockerfile.tf
+++ docker/dockerfile.tf
@@ -71,8 +71,9 @@ RUN if [ "$HUGECTR_DEV_MODE" == "false" ]; then \
 
 # Install distributed-embeddings
 ARG INSTALL_DISTRIBUTED_EMBEDDINGS=true
+ARG TFDE_VER=main
 RUN if [ "$INSTALL_DISTRIBUTED_EMBEDDINGS" == "true" ]; then \
-        git clone https://github.com/NVIDIA-Merlin/distributed-embeddings.git /distributed_embeddings/ && \
+        git clone --recurse-submodules https://github.com/NVIDIA-Merlin/distributed-embeddings.git /distributed_embeddings/ && \
         cd /distributed_embeddings && git checkout ${TFDE_VER} && \
         make pip_pkg && pip install artifacts/*.whl && make clean; \
     fi
