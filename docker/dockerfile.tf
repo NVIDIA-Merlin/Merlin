3f7b7ae4e21a2a5a6a9be1034dd913091c6a8a7e
Deyu Fu <deyuf@nvidia.com>
2022-10-10 14:58:45 +0800

    switch to use fixed version tag

--- docker/dockerfile.tf
+++ docker/dockerfile.tf
@@ -71,9 +71,9 @@ RUN if [ "$HUGECTR_DEV_MODE" == "false" ]; then \
 
 # Install distributed-embeddings
 ARG INSTALL_DISTRIBUTED_EMBEDDINGS=true
-ARG TFDE_VER=main
+ARG TFDE_VER=v0.2
 RUN if [ "$INSTALL_DISTRIBUTED_EMBEDDINGS" == "true" ]; then \
-        git clone --recurse-submodules https://github.com/NVIDIA-Merlin/distributed-embeddings.git /distributed_embeddings/ && \
-        cd /distributed_embeddings && git checkout ${TFDE_VER} && \
+        git clone https://github.com/NVIDIA-Merlin/distributed-embeddings.git /distributed_embeddings/ && \
+        cd /distributed_embeddings && git checkout ${TFDE_VER} && git submodule update --init --recursive && \
         make pip_pkg && pip install artifacts/*.whl && make clean; \
     fi
