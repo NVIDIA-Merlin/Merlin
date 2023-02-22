bd7fe3b4ca0198b541e76bd16cf83546d622b9c8
Karl Higley <kmhigley@gmail.com>
2023-02-22 12:07:52 -0500

    Remove the container size check from the container software check

--- ci/container_software.sh
+++ ci/container_software.sh
@@ -6,14 +6,6 @@ devices=$2
 exit_code=0
 
 echo "##################"
-echo "# Container size #"
-echo "##################"
-
-cd / && NUMGB=$(du -sh 2> /dev/null | grep -oE '[0-9]*G' | grep -oE '[0-9]*') 
-if [ $NUMGB -ge 15  ]; then echo "Size of container exceeds 15GB, failed build." && exit 1 ; fi;
-
-
-echo "##################"
 echo "# Software check #"
 echo "##################"
 
