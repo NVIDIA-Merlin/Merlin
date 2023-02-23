f887eafdebba7ef8244ed87d2a9f613aebbd4f8a
Julio <jperez@nvidia.com>
2023-02-22 21:55:01 -0500

    fix size check to exclude mounts

--- ci/container_software.sh
+++ ci/container_software.sh
@@ -9,7 +9,8 @@ echo "##################"
 echo "# Container size #"
 echo "##################"
 
-cd / && NUMGB=$(du -sh 2> /dev/null | grep -oE '[0-9]*G' | grep -oE '[0-9]*') 
+cd / && NUMGB=$(du -sh --exclude "raid" 2> /dev/null | grep -oE '[0-9]*G' | grep -oE '[0-9]*') 
+echo "Size of container is: $NUMGB GB"
 if [ $NUMGB -ge 15  ]; then echo "Size of container exceeds 15GB, failed build." && exit 1 ; fi;
 
 
