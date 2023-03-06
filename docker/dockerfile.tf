7699da74edecb88188409de81c72a551a5492ed8
Emma <qqiao@nvidia.com>
2023-03-06 21:54:39 +0800

    resolve chery pick merge conflict with fd6bef16d0c2d83e9eed5eb5d31da2320c2e0d59

--- docker/dockerfile.tf
+++ docker/dockerfile.tf
@@ -16,7 +16,7 @@ COPY --chown=1000:1000 --from=triton /opt/tritonserver/backends/tensorflow2 back
 
 # Tensorflow dependencies (only)
 # Pinning to pass hugectr sok tests
-RUN pip install --no-cache-dir tensorflow-gpu==2.9.2 \
+RUN pip install --no-cache-dir tensorflow-gpu==2.9.2 protobuf==3.20.3 \
     && pip uninstall tensorflow-gpu keras -y
 
 # DLFW Tensorflow packages
@@ -33,29 +33,17 @@ COPY --chown=1000:1000 --from=dlfw /usr/local/bin/horovodrun /usr/local/bin/horo
 # Need to install transformers after tensorflow has been pulled in, so it builds artifacts correctly.
 RUN pip install --no-cache-dir transformers==4.23.1
 
-# Install dependencies for hps tf plugin
-RUN apt update -y --fix-missing && \
-    apt install -y --no-install-recommends \
-        #   Required to build RocksDB.
-            libgflags-dev \
-            zlib1g-dev libbz2-dev libsnappy-dev liblz4-dev libzstd-dev \
-        #   Required to build RdKafka.
-            zlib1g-dev libzstd-dev \
-            libssl-dev libsasl2-dev && \
-    apt clean && \
-    rm -rf /var/lib/apt/lists/*
-
-
 # Install HugeCTR
-ENV LD_LIBRARY_PATH=/usr/local/hugectr/lib:$LD_LIBRARY_PATH \
-    LIBRARY_PATH=/usr/local/hugectr/lib:$LIBRARY_PATH \
-    SOK_COMPILE_UNIT_TEST=ON
-
 # Arguments "_XXXX" are only valid when $HUGECTR_DEV_MODE==false
 ARG HUGECTR_DEV_MODE=false
 ARG _HUGECTR_REPO="github.com/NVIDIA-Merlin/HugeCTR.git"
 ARG _CI_JOB_TOKEN=""
 ARG HUGECTR_VER=main
+
+ENV CPATH=$CPATH:${HUGECTR_HOME}/include \
+    LD_LIBRARY_PATH=${HUGECTR_HOME}/lib:/usr/local/lib/python3.8/dist-packages/tensorflow:$LD_LIBRARY_PATH \
+    LIBRARY_PATH=${HUGECTR_HOME}/lib:$LIBRARY_PATH \
+    SOK_COMPILE_UNIT_TEST=ON
 
 RUN mkdir -p /usr/local/nvidia/lib64 && \
     ln -s /usr/local/cuda/lib64/libcusolver.so /usr/local/nvidia/lib64/libcusolver.so.10
@@ -69,17 +57,21 @@ RUN if [ "$HUGECTR_DEV_MODE" == "false" ]; then \
         git clone --branch ${HUGECTR_VER} --depth 1 --recurse-submodules --shallow-submodules https://${_CI_JOB_TOKEN}${_HUGECTR_REPO} /hugectr && \
         pushd /hugectr && \
         rm -rf .git/modules && \
-	pip install --no-cache-dir ninja && \
+        pip --no-cache-dir install ninja tf2onnx && \
         # Install SOK
         cd sparse_operation_kit && \
         python setup.py install && \
         # Install HPS TF plugin
         cd ../hps_tf && \
         python setup.py install && \
-        popd; \
+        popd &&\
+	mv /hugectr/ci ~/hugectr-ci && mv /hugectr/sparse_operation_kit ~/hugectr-sparse_operation_kit && \
+    	rm -rf /hugectr && mkdir -p /hugectr && \
+        mv ~/hugectr-ci /hugectr/ci && mv ~/hugectr-sparse_operation_kit /hugectr/sparse_operation_kit; \
     fi; \
     if [ "$INSTALL_DISTRIBUTED_EMBEDDINGS" == "true" ]; then \
         git clone --branch ${TFDE_VER} --depth 1 https://github.com/NVIDIA-Merlin/distributed-embeddings.git /distributed_embeddings/ && \
         cd /distributed_embeddings && git submodule update --init --recursive && \
         make pip_pkg && pip install --no-cache-dir artifacts/*.whl && make clean; \
-    fi; \
+    fi; 
+
