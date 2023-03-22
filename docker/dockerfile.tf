3e9593fac1af552fba6fa34057a9bd68d0a2188b
Emma <qqiao@nvidia.com>
2023-03-22 21:33:51 +0800

    Fix libnvinfer conflict (#855)
    * Fix conflict for libnvinfer with upstream
    
    * Correct copy path of libcupti.so
    
    * Update distributed-embeddings to latest tag
    
    * Remove jdk and mvn which are handled by hugectr build script
    
    * Upgrade cupy-cuda12x due to SOK error
    
    ---------
    
    Co-authored-by: Karl Higley <karlb@nvidia.com>

--- docker/dockerfile.tf
+++ docker/dockerfile.tf
@@ -52,7 +52,7 @@ RUN ln -s /usr/lib/x86_64-linux-gnu/libibverbs.so.1 /usr/lib/x86_64-linux-gnu/li
 
 # Install distributed-embeddings and sok
 ARG INSTALL_DISTRIBUTED_EMBEDDINGS=true
-ARG TFDE_VER=v0.2
+ARG TFDE_VER=v0.3
 RUN if [ "$HUGECTR_DEV_MODE" == "false" ]; then \
         git clone --branch ${HUGECTR_VER} --depth 1 --recurse-submodules --shallow-submodules https://${_CI_JOB_TOKEN}${_HUGECTR_REPO} /hugectr && \
         pushd /hugectr && \
