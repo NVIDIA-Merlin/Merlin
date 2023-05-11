a81581491e76289f9023e0492e2a3bab40330198
root <ronayak@hotmail.com>
2023-05-11 22:47:44 +0000

    update

--- examples/quick_start/scripts/inference/args_parsing.py
+++ examples/quick_start/scripts/inference/args_parsing.py
@@ -3,7 +3,7 @@ import argparse
 
 def build_arg_parser():
     parser = argparse.ArgumentParser(
-        description="Inference script for deploying ranking models with NVTabular on TIS"
+        description="Inference script for deploying ranking models with NVTabular on Triton"
     )
 
     # Inputs / Outputs
@@ -15,33 +15,13 @@ def build_arg_parser():
     parser.add_argument(
         "--load_model_path",
         default=None,
-        help="If provided, loads a model saved by --save_model_path "
-        "instead of initializing the parameters randomly",
+        help="Loads a model from its saved path. ",
     )
     parser.add_argument(
         "--ensemble_export_path",
         default="./models",
         help="Path for exporting the model artifacts to "
-        "load them on Triton IS afterwards.",
-    )
-
-    # parser.add_argument("--groupby_feature", default="", help="")
-
-    parser.add_argument(
-        "--to_int32", default="", help="Cast these columns (comma-sep) to int32."
-    )
-    parser.add_argument(
-        "--to_int16",
-        default="",
-        help="Cast these columns (comma-sep) to int16, to save some memory.",
-    )
-    parser.add_argument(
-        "--to_int8",
-        default="",
-        help="Cast these columns (comma-sep) to int32, to save some memory.",
-    )
-    parser.add_argument(
-        "--to_float32", default="", help="Cast these columns (comma-sep) to float32"
+        "load them on Triton inference server afterwards.",
     )
 
     return parser
--- examples/quick_start/scripts/inference/inference.py
+++ examples/quick_start/scripts/inference/inference.py
@@ -1,13 +1,14 @@
-import logging
 import os
+import logging
 import shutil
+import numpy as np
 
 import tensorflow as tf
-from merlin.schema.tags import Tags
 from merlin.systems.dag.ensemble import Ensemble
 from merlin.systems.dag.ops.tensorflow import PredictTensorflow
 from merlin.systems.dag.ops.workflow import TransformWorkflow
 from nvtabular.workflow import Workflow
+import merlin.models.tf as mm
 
 from args_parsing import parse_arguments
 
@@ -16,22 +17,17 @@ def main(args):
     logging.basicConfig(level=logging.INFO)
 
     workflow_stored_path = os.path.join(args.nvt_workflow_path, "workflow")
-    logging.info(f"loading nvt workflow from: {workflow_stored_path}")
+    logging.info(f"Loading nvt workflow from: {workflow_stored_path}")
     workflow = Workflow.load(workflow_stored_path)
-
-    logging.info("removing target columns from workflow input schema")
-    label_columns = workflow.output_schema.select_by_tag(Tags.TARGET).column_names
-    workflow.remove_inputs(label_columns)
-    logging.info("printing out workflow input schema")
-    logging.info(workflow.input_schema)
-    logging.info("printing out workflow output schema")
-    logging.info(workflow.output_schema)
+    logging.info("Printing out workflow input schema")
+    logging.info(workflow.input_schema.column_names)
 
     # load the tensorflow model
     tf_model_path = args.load_model_path
-    logging.info(f"loading saved ranking model from: {tf_model_path}")
+    logging.info(f"Loading saved ranking model from: {tf_model_path}")
     model = tf.keras.models.load_model(tf_model_path)
 
+    logging.info("Creating ensemble graph")
     # create ensemble graph
     serving_operators = (
         workflow.input_schema.column_names
@@ -43,15 +39,20 @@ def main(args):
 
     ens_model_path = args.ensemble_export_path
 
-    # Make sure we have a clean stats space
+    logging.info("Removing the existing ensemble model path and creating a new folder")
     if os.path.isdir(ens_model_path):
         shutil.rmtree(ens_model_path)
     os.mkdir(ens_model_path)
 
-    # store the exported models and the config files.
+    logging.info(f"Exporting model artifacts to: {ens_model_path}")
     ens_conf, node_confs = ensemble.export(ens_model_path)
+    
+    logging.info("Saving model output names to disk")
+    outputs = ensemble.graph.output_schema.column_names
 
-    logging.info("Finished exporting triton models and config files")
+    np.array(outputs).dump(open('outputs.npy', 'wb'))
+
+    logging.info("Finished exporting models and config files")
 
 
 if __name__ == "__main__":
