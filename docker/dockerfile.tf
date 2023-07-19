cc863d7f4b40d9c2b35ba6c3add8ad0579a9c585
Matthias Langer <bashimao@users.noreply.github.com>
2023-07-19 09:29:38 +0800

    Enable cross-compiling for x86 + NVIDIA Grace (ARM64) (#1049)
    * Update the dockerfile for new upstream
    
    * Updates to make build compatible with 23.05 base image.
    
    * Simplified build process for HugeCTR training image.
    
    * Update tf and torch dockerfile for new upstream image
    
    * Remove env PYTHON_VERSION since it's already in base.
    
    * Tick base image up to 23.06, fix `tritonclient` dependency.
    
    * Update base to 23.06
    
    * Tick up base image version.
    
    * Merge branch 'main' into fix-update_base_23.05
    
    * Allow cross-compiling on x86 + NVIDIA Grace (ARM64).
    
    * Reverse two wrongful changes.
    
    * Update TF dockerfile for x86 + Grace/ARM64 cross compile.
    
    * Just add an empty linefor symmetry reasons.
    
    ---------
    
    Co-authored-by: qqiao <qqiao@nvidia.com>
    Co-authored-by: Karl Higley <karlb@nvidia.com>
    Co-authored-by: Julio Perez <37191411+jperez999@users.noreply.github.com>

--- docker/dockerfile.tf
+++ docker/dockerfile.tf
@@ -41,19 +41,19 @@ ARG _HUGECTR_REPO="github.com/NVIDIA-Merlin/HugeCTR.git"
 ARG _CI_JOB_TOKEN=""
 ARG HUGECTR_VER=main
 
-ENV CPATH=$CPATH:${HUGECTR_HOME}/include \
-    LD_LIBRARY_PATH=${HUGECTR_HOME}/lib:/usr/local/lib/python${PYTHON_VERSION}/dist-packages/tensorflow:$LD_LIBRARY_PATH \
+ENV LD_LIBRARY_PATH=/usr/local/lib/python${PYTHON_VERSION}/dist-packages/tensorflow:$LD_LIBRARY_PATH \
     LIBRARY_PATH=${HUGECTR_HOME}/lib:$LIBRARY_PATH \
     SOK_COMPILE_UNIT_TEST=ON
 
 RUN mkdir -p /usr/local/nvidia/lib64 && \
-    ln -s /usr/local/cuda/lib64/libcusolver.so /usr/local/nvidia/lib64/libcusolver.so.10
+    ln -s /usr/local/cuda/lib64/libcusolver.so /usr/local/nvidia/lib64/libcusolver.so
 
-RUN ln -s /usr/lib/x86_64-linux-gnu/libibverbs.so.1 /usr/lib/x86_64-linux-gnu/libibverbs.so
+RUN ln -s libibverbs.so.1 $(find /usr/lib/*-linux-gnu/libibverbs.so.1 | sed -e 's/\.1$//g')
 
 # Install distributed-embeddings and sok
 ARG INSTALL_DISTRIBUTED_EMBEDDINGS=false
-ARG TFDE_VER=v0.3
+ARG TFDE_VER=v23.03.00
+
 RUN if [ "$HUGECTR_DEV_MODE" == "false" ]; then \
         git clone --branch ${HUGECTR_VER} --depth 1 --recurse-submodules --shallow-submodules https://${_CI_JOB_TOKEN}${_HUGECTR_REPO} /hugectr && \
         pushd /hugectr && \
@@ -65,14 +65,14 @@ RUN if [ "$HUGECTR_DEV_MODE" == "false" ]; then \
         # Install HPS TF plugin
         cd ../hps_tf && \
         python setup.py install && \
-        popd &&\
-	mv /hugectr/ci ~/hugectr-ci && mv /hugectr/sparse_operation_kit ~/hugectr-sparse_operation_kit && \
+        popd && \
+        mv /hugectr/ci ~/hugectr-ci && mv /hugectr/sparse_operation_kit ~/hugectr-sparse_operation_kit && \
     	rm -rf /hugectr && mkdir -p /hugectr && \
-        mv ~/hugectr-ci /hugectr/ci && mv ~/hugectr-sparse_operation_kit /hugectr/sparse_operation_kit; \
-    fi && \
+        mv ~/hugectr-ci /hugectr/ci && mv ~/hugectr-sparse_operation_kit /hugectr/sparse_operation_kit \
+    ; fi && \
     if [ "$INSTALL_DISTRIBUTED_EMBEDDINGS" == "true" ]; then \
         git clone --branch ${TFDE_VER} --depth 1 https://github.com/NVIDIA-Merlin/distributed-embeddings.git /distributed_embeddings/ && \
         cd /distributed_embeddings && git submodule update --init --recursive && \
-        make pip_pkg && pip install --no-cache-dir artifacts/*.whl && make clean; \
-    fi; 
+        make pip_pkg && pip install --no-cache-dir artifacts/*.whl && make clean \
+    ; fi
 
