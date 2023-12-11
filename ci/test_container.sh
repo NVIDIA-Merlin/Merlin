49ceb04ddecd13102eb1fa59ec7d1b47053d106f
qqiao <qqiao@nvidia.com>
2023-12-10 19:21:08 -0800

    Skip other merlin components tests

--- ci/test_container.sh
+++ ci/test_container.sh
@@ -17,6 +17,8 @@ if [ $container != 'merlin-ci-runner' ]; then
 fi
 
 ${ci_script_dir}container_software.sh $container $devices
-${ci_script_dir}container_integration.sh $container $devices $suppress_failures
-${ci_script_dir}container_unit.sh $container $devices
+if [ $MERLIN_BASE_MIN != "true" ]; then
+    ${ci_script_dir}container_integration.sh $container $devices $suppress_failures
+    ${ci_script_dir}container_unit.sh $container $devices
+fi
 
